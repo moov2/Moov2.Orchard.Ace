1f34b2459092d087975acbfed2052feceac56996
Peter Keating <peter@moov2.com>
2017-02-25 20:53:47 +0000

    Setup JS / CSS files for Ace and embedding Ace

---
+++ Assets/JavaScript/orchard.ace.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    var editor, session, inputElement;
+
+    /**
+     * Filters out unwanted annotations (e.g. warning about doctype) due to common
+     * case is the HTML being edited is not a full HTML document but a piece of HTML.
+     */
+    var filterAnnotation = function () {
+        var annotations = session.getAnnotations() || [], i = len = annotations.length;
+
+        while (i--) {
+            if (/doctype first\. Expected/.test(annotations[i].text)) {
+                annotations.splice(i, 1);
+            }
+        }
+
+        if (len > annotations.length) {
+            session.setAnnotations(annotations);
+        }
+    };
+
+    /**
+     * Initialises Ace editor.
+     */
+    var initialise = function () {
+        editor = ace.edit('editor'),
+        session = editor.getSession(),
+        inputElement = document.querySelector('#Text');
+
+        editor.setTheme('ace/theme/monokai');
+        editor.setShowPrintMargin(false);
+
+        session.setMode('ace/mode/html');
+        session.setUseWrapMode(true);
+
+        session.on('changeAnnotation', filterAnnotation);
+        session.on('change', update);
+    };
+    
+    /**
+     * Updates hidden HTML input with latest content from Ace editor.
+     */
+    var update = function () {
+        inputElement.value = session.getValue();
+    };
+
+    document.addEventListener('DOMContentLoaded', initialise);
+})();
---
+++ ResourceManifest.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.UI.Resources;
+
+namespace Moov2.Orchard.Ace {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+            manifest.DefineStyle("AceStyles").SetUrl("Styles.css");
+            manifest.DefineScript("AceVendorJs").SetUrl("ace/ace.js").SetCdn("//cdnjs.cloudflare.com/ajax/libs/ace/1.2.6/ace.js");
+            manifest.DefineScript("AceJs").SetUrl("orchard.ace.min.js", "orchard.ace.js").SetDependencies(new string[] { "AceVendorJs" });
+        }
+    }
+}
